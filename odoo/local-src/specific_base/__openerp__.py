8a2efa66efe45ac82403c6828a1761881d8c1d87
Damien Crier <damien.crier@camptocamp.com>
2016-08-12 17:52:42 +0200

    add expiry_date computation + base action rule to cancel SO after 90 days

--- odoo/local-src/specific_base/__openerp__.py
+++ odoo/local-src/specific_base/__openerp__.py
@@ -28,6 +28,8 @@
           'views/res_partner.xml',
           'views/survey_templates.xml',
           'views/sale_order.xml',
+          'data/ir_actions_server.xml',
+          'data/base_action_rule.xml',
           ],
  'installable': True,
  }
--- odoo/local-src/specific_base/models/__init__.py
+++ odoo/local-src/specific_base/models/__init__.py
@@ -4,3 +4,5 @@ from . import company_schedule
 from . import company_phototherapist
 from . import res_company
 from . import res_partner
+from . import sale_order
+from . import calendar_event
