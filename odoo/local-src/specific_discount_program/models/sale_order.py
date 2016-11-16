5bfdcbe56d13707fc12e8d57412ceee03c3318ad
Cyril Gaudin <cyril.gaudin@camptocamp.com>
2016-11-16 14:59:10 +0100

    #182: Voucher are generate at sale.order confirmation.

--- odoo/local-src/specific_discount_program/models/sale_order.py
+++ odoo/local-src/specific_discount_program/models/sale_order.py
@@ -2,22 +2,100 @@
 # © 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+from openerp import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    is_sponsored = fields.Boolean(
+        compute='_compute_is_sponsored'
+    )
+
+    generated_voucher_ids = fields.One2many(
+        comodel_name='sale.discount.program',
+        inverse_name='source_sale_id'
+    )
+
+    @api.depends(
+        'pricelist_id',
+        'partner_id', 'partner_id.sponsor_id', 'partner_id.sponsor_id.active'
+    )
+    def _compute_is_sponsored(self):
+        sponsor_pricelist = self.env.ref('scenario.pricelist_sponsorship')
+        for sale in self:
+            if not sponsor_pricelist:
+                sale.is_sponsored = False
+            else:
+                sale.is_sponsored = sale.partner_id.sponsor_id.active \
+                                    and sale.pricelist_id == sponsor_pricelist
+
     @api.multi
-    def _prepare_invoice(self):
-        """ Specified on invoice if sponsorship program was used in sale.order
+    def create_partner_voucher(self, partner_id):
+        self.ensure_one()
+        months_validity = int(self.env['ir.config_parameter'].get_param(
+            'voucher_default_validity', '0'
+        ))
+
+        expiration_date = None
+        if months_validity:
+            expiration_date = date.today() + relativedelta(
+                months=months_validity
+            )
+
+        self.sudo().write({
+            'generated_voucher_ids': [(0, False, {
+                'partner_id': partner_id,
+                'combinable': False,
+                'voucher_code': self.env['ir.sequence'].next_by_code(
+                    'discount.program.voucher_code'
+                ),
+                'voucher_amount': self.get_voucher_amount(),
+                'max_use': 1,
+                'expiration_date': expiration_date,
+            })]
+        })
+
+    @api.multi
+    def get_voucher_amount(self):
+        """ Compute the amount for the voucher based on invoice amount.
         """
         self.ensure_one()
-        vals = super(SaleOrder, self)._prepare_invoice()
+        icp = self.env['ir.config_parameter']
+        percent = float(icp.get_param('voucher_percent', '10'))
+        max_amount = int(icp.get_param('voucher_max_amount', '100'))
 
-        vals['sale_partner_id'] = self.partner_id.id
+        amount = min(
+            self.amount_total * percent / 100,
+            max_amount
+        )
+        return amount
 
-        if self.pricelist_id == self.env.ref('scenario.pricelist_sponsorship'):
-            vals['sponsor_id'] = self.partner_id.sponsor_id.id
+    @api.multi
+    def action_confirm(self):
+        super(SaleOrder, self).action_confirm()
 
-        return vals
+        for sale in self:
+            # Bon d'achat si la commande a utilisé le programme de
+            # parainnage et si le parrain est toujours valide
+            if sale.is_sponsored:
+                sale.create_partner_voucher(
+                    sale.partner_id.sponsor_id.partner_id.id
+                )
+
+            # Bon d'achat pour chaque commande
+            sale.create_partner_voucher(sale.partner_id.id)
+
+    @api.multi
+    def action_cancel(self):
+        result = super(SaleOrder, self).action_cancel()
+
+        # Delete unused vouchers
+        for program in self.mapped('generated_voucher_ids'):
+            if program.nb_use == 0:
+                program.sudo().unlink()
+
+        return result
--- odoo/local-src/specific_discount_program/tests/test_program.py
+++ odoo/local-src/specific_discount_program/tests/test_program.py
@@ -2,8 +2,7 @@
 # © 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp.exceptions import UserError
-from openerp.tests.common import TransactionCase
-
+from openerp.tests.common import TransactionCase, post_install, at_install
 
 class TestProgram(TransactionCase):
 
@@ -38,6 +37,11 @@ class TestProgram(TransactionCase):
                 'res_id': self.sponsor_pricelist.id,
             })
 
+        self.phototherapist = self.env['res.company.phototherapist'].create({
+            'name': 'Unittest phototherapist',
+            'company_id': self.env.user.company_id.id
+        })
+
         self.p1 = self.product_model.create({'name': 'Unittest P1'})
 
         self.partner1 = self.partner_model.create({
@@ -48,13 +52,20 @@ class TestProgram(TransactionCase):
         self.assertEqual(0, self.sponsor_model.search_count([]))
 
         self.sale_sponsor = self.sale_model.create({
+            'phototherapist_id': self.phototherapist.id,
             'partner_id': self.partner1.id,
             'order_line': [(0, 0, {
                 'product_id': self.p1.id, 'product_uom_qty': 1
             })]
         })
         self.sale_sponsor.action_confirm()
+        created_voucher = self.program_model.search([
+            ('partner_id', '=', self.partner1.id)
+        ])
+        created_voucher.unlink()
 
+    @post_install(True)
+    @at_install(False)
     def test_vouchers(self):
         sponsor = self.sponsor_model.search([])
         self.assertEqual(1, len(sponsor))
@@ -69,6 +80,7 @@ class TestProgram(TransactionCase):
 
         # Simulate sponsor program by setting pricelist_id to sponsor pricelist
         sale = self.sale_model.create({
+            'phototherapist_id': self.phototherapist.id,
             'partner_id': partner2.id,
             'pricelist_id': self.sponsor_pricelist.id,
             'order_line': [(0, 0, {
@@ -76,13 +88,6 @@ class TestProgram(TransactionCase):
             })]
         })
         sale.action_confirm()
-
-        invoice_id = sale.action_invoice_create()
-        invoice = self.invoice_model.browse(invoice_id)
-
-        self.assertEqual(self.partner1, invoice.sponsor_id.partner_id)
-
-        invoice.confirm_paid()
 
         self.assertEqual(2, self.program_model.search_count([
             ('voucher_code', '!=', False)
@@ -104,8 +109,8 @@ class TestProgram(TransactionCase):
         ])
         sponsor_voucher.nb_use = 1
 
-        # Refund the invoice
-        invoice.refund()
+        # Cancel the sale.order
+        sale.action_cancel()
 
         # Unused voucher has been deleted
         self.assertEqual(1, self.program_model.search_count([
@@ -120,6 +125,71 @@ class TestProgram(TransactionCase):
             ('partner_id', '=', self.partner1.id)
         ]))
 
+        # Another sale for partner 2 will create only one voucher
+        sale = self.sale_model.create({
+            'phototherapist_id': self.phototherapist.id,
+            'partner_id': partner2.id,
+            'order_line': [(0, 0, {
+                'product_id': self.p1.id, 'product_uom_qty': 1,
+            })]
+        })
+        sale.action_confirm()
+
+        self.assertEqual(2, self.program_model.search_count([
+            ('voucher_code', '!=', False)
+        ]))
+
+        self.assertEqual(1, self.program_model.search_count([
+            ('partner_id', '=', partner2.id)
+        ]))
+
+        self.assertEqual(1, self.program_model.search_count([
+            ('partner_id', '=', self.partner1.id)
+        ]))
+
+    @post_install(True)
+    @at_install(False)
+    def test_voucher_cancel_both(self):
+        sponsor = self.sponsor_model.search([])
+        partner2 = self.partner_model.create({
+            'name': 'Other partner',
+            'sponsor_id': sponsor.id,
+        })
+
+        sale = self.sale_model.create({
+            'phototherapist_id': self.phototherapist.id,
+            'partner_id': partner2.id,
+            'pricelist_id': self.sponsor_pricelist.id,
+            'order_line': [(0, 0, {
+                'product_id': self.p1.id, 'product_uom_qty': 1,
+            })]
+        })
+        sale.action_confirm()
+
+        self.assertEqual(2, self.program_model.search_count([
+            ('voucher_code', '!=', False)
+        ]))
+
+        # One voucher for the client
+        self.assertEqual(1, self.program_model.search_count([
+            ('partner_id', '=', partner2.id)
+        ]))
+
+        # One voucher for the sponsor
+        self.assertEqual(1, self.program_model.search_count([
+            ('partner_id', '=', self.partner1.id)
+        ]))
+
+        # Cancel the sale.order
+        sale.action_cancel()
+
+        # Unused voucher has been deleted
+        self.assertEqual(0, self.program_model.search_count([
+            ('voucher_code', '!=', False)
+        ]))
+
+    @post_install(True)
+    @at_install(False)
     def test_sponsor_became_invalid(self):
         sponsor = self.sponsor_model.search([])
         self.assertEqual(1, len(sponsor))
@@ -131,24 +201,19 @@ class TestProgram(TransactionCase):
 
         # Simulate sponsor program by setting pricelist_id to sponsor pricelist
         sale = self.sale_model.create({
+            'phototherapist_id': self.phototherapist.id,
             'partner_id': partner2.id,
             'pricelist_id': self.sponsor_pricelist.id,
             'order_line': [(0, 0, {
                 'product_id': self.p1.id, 'product_uom_qty': 1
             })]
         })
-        sale.action_confirm()
-
-        invoice_id = sale.action_invoice_create()
-        invoice = self.invoice_model.browse(invoice_id)
-
-        self.assertEqual(self.partner1, invoice.sponsor_id.partner_id)
 
         # Sponsor sale is cancelled so this partner can not be a sponsor
         # anymore
         self.sale_sponsor.action_cancel()
 
-        invoice.confirm_paid()
+        sale.action_confirm()
 
         # Voucher for client
         self.assertEqual(1, self.program_model.search_count([
@@ -160,6 +225,8 @@ class TestProgram(TransactionCase):
             ('partner_id', '=', self.partner1.id)
         ]))
 
+    @post_install(True)
+    @at_install(False)
     def test_voucher_limit(self):
         icp = self.env['ir.config_parameter']
         icp.set_param('voucher_max_count', 2)
@@ -183,6 +250,7 @@ class TestProgram(TransactionCase):
         })
 
         sale = self.sale_model.create({
+            'phototherapist_id': self.phototherapist.id,
             'partner_id': self.partner1.id,
             'program_code_ids': [(6, False, [v1.id, v2.id, v3.id])],
             'order_line': [(0, 0, {
