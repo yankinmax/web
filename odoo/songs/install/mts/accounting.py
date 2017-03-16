ee027513e67a6c4f2645f4fcc5067289c40194c6
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-03-16 11:16:32 +0100

    Initial setup

---
+++ odoo/songs/install/mts/accounting.py
@@ -0,0 +1,26 @@
+
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+import anthem
+from anthem.lyrics.records import create_or_update
+
+
+@anthem.log
+def set_fiscalyear(ctx):
+    values = {'date_start': '2017-01-01',
+              'name': '2017',
+              'date_end': '2017-12-31',
+              'type_id': 1,
+              'company_id': ctx.env.ref('__setup__.company_mte').id,
+              'active': True,
+              }
+    create_or_update(ctx, 'date.range',
+                     '__setup__.date_range_mte_2017', values)
+
+
+@anthem.log
+def main(ctx):
+    """ Configuring accounting """
+    set_fiscalyear(ctx)
--- odoo/songs/install/pre.py
+++ odoo/songs/install/pre.py
@@ -2,46 +2,27 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from base64 import b64encode
-from pkg_resources import resource_string
+# from base64 import b64encode
+from pkg_resources import resource_stream
 
+import os
 import anthem
 
-from anthem.lyrics.records import create_or_update
+from anthem.lyrics.loaders import load_csv_stream
 
 from ..common import req
 
 
 @anthem.log
 def setup_company(ctx):
-    """ Setup company """
-    company = ctx.env.ref('base.main_company')
-    company.name = 'MTS SA'
-
+    """ Setup companies """
     # load logo on company
-    logo_content = resource_string(req, 'data/images/company_main_logo.png')
-    b64_logo = b64encode(logo_content)
-    company.logo = b64_logo
+    # logo_content = resource_string(req, 'data/images/company_main_logo.png')
+    # b64_logo = b64encode(logo_content)
+    # company.logo = b64_logo
 
-    with ctx.log(u'Configuring company'):
-        values = {
-            'name': "Metallo Tests SA",
-            'street': "",
-            'zip': "",
-            'city': "",
-            'country_id': ctx.env.ref('base.ch').id,
-            'phone': "+41 00 000 00 00",
-            'fax': "+41 00 000 00 00",
-            'email': "contact@mtsmte.ch",
-            'website': "http://www.mtsmte.ch",
-            'vat': "VAT",
-            'parent_id': company.id,
-            'logo': b64_logo,
-            'currency_id': ctx.env.ref('base.CHF').id,
-        }
-        create_or_update(ctx, 'res.company',
-                         'scenario.mtsmte_ch',
-                         values)
+    content = resource_stream(req, 'data/install/res.company.csv')
+    load_csv_stream(ctx, 'res.company', content, delimiter=',')
 
 
 @anthem.log
@@ -56,11 +37,16 @@ def setup_language(ctx):
 
 
 @anthem.log
-def set_web_base_url(ctx):
-    """ Configuring web.base.url """
-    url = 'http://localhost:8069'
-    ctx.env['ir.config_parameter'].set_param('web.base.url', url)
-    ctx.env['ir.config_parameter'].set_param('web.base.url.freeze', 'True')
+def admin_user_password(ctx):
+    """ Changing admin password """
+    if os.environ.get('RUNNING_ENV') == 'dev':
+        ctx.log_line('Not changing password for dev RUNNING_ENV')
+        return
+    ctx.env.user.password_crypt = (
+        '$pbkdf2-sha512$19000$/v9f6/3/fw8BoFSqdW4t5Q$qT96ieTiXgNT8I/ePs2zd'
+        'oAc66EVy8IjToQxUxOaXVwKLwCGWY9EBu3TtpSvqzcTqnULPNOGH63x4j261Aia7A'
+
+    )
 
 
 @anthem.log
@@ -68,4 +54,4 @@ def main(ctx):
     """ Main: creating demo data """
     setup_company(ctx)
     setup_language(ctx)
-    set_web_base_url(ctx)
+    admin_user_password(ctx)
