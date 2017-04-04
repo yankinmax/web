f35c256e54fa1a0fedba17176f3c09dc4fe5ea13
Denis Leemann <denis.leemann@camptocamp.com>
2017-04-04 13:53:17 +0200

    project stages

---
+++ odoo/local-src/mtsmte_project/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MTSMTE Specific Project Module",
+    "version": "10.0.1.0.0",
+    "depends": [
+        'project',
+        'project_task_default_stage',
+    ],
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "website": "http://www.camptocamp.com",
+    "license": "GPL-3",
+    "category": "Project",
+    "data": [
+        # data
+        'data/project_task.xml',
+    ],
+    'installable': True,
+}
