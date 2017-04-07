34017846e1f548d6e0f5ae31cd42859eac410549
Denis Leemann <denis.leemann@camptocamp.com>
2017-04-07 14:49:58 +0200

    on_change_product_id on sale_order_lines

--- odoo/local-src/mtsmte_sale/models/sale_order_line.py
+++ odoo/local-src/mtsmte_sale/models/sale_order_line.py
@@ -3,7 +3,7 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SaleOrderLine(models.Model):
@@ -18,3 +18,8 @@ class SaleOrderLine(models.Model):
         related='product_id.is_chemistry',
         readonly=True,
     )
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        self.product_substance_ids = False
+        self.product_substance_ids = self.product_id.product_substance_ids
