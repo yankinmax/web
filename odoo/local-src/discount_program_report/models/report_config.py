99cb723b7f110de741b2780858fbbac7c0e5aa73
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-06-26 14:08:12 +0200

    Use one image and improve configuration

--- odoo/local-src/discount_program_report/models/report_config.py
+++ odoo/local-src/discount_program_report/models/report_config.py
@@ -2,37 +2,81 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, fields, api, _
 
 
 class SaleDiscountProgramReportConfig(models.Model):
 
     _name = 'sale.discount.program.report.config'
 
+    name = fields.Char('Name')
     lang_id = fields.Many2one('res.lang', 'Language', required=True)
 
-    program_type = fields.Selection([('voucher', 'Voucher'),
-                                     ('sponsorship', 'Sponsorship')],
-                                    'Program type', required=True)
+    active = fields.Boolean('Active')
 
-    top_image = fields.Binary('Top image')
-    left_image = fields.Binary('Left image')
-    right_image = fields.Binary('Right image')
-    bottom_image = fields.Binary('Bottom image')
+    top_image = fields.Binary('Top image',
+                              help='This image will be displayed with a width '
+                                   'of 1083px at the top of the report')
+
+    amount_zone_width = fields.Integer('Amount zone width (in px)',
+                                       help='This is the width of the amount '
+                                            'zone in the top picture ')
+    amount_position_top = fields.Integer('Amount zone position top (in px)',
+                                         help='This is the position in pixels '
+                                              'from the top of the image '
+                                              'where the amount zone is '
+                                              'displayed.')
+    amount_position_left = fields.Integer('Amount zone position left (in px)',
+                                          help='This is the position in pixels'
+                                               ' from the left of the image '
+                                               'where the amount zone is '
+                                               'displayed.'
+                                          )
+
+    amount_container_style = fields.Char(
+        compute='_compute_amount_container_style')
 
     ribbon_text = fields.Char('Ribbon text')
 
-    general_terms_conditions_title = fields.Html('General terms and conditions title')
-    general_terms_conditions_left = fields.Html('General terms and conditions left column')
+    general_terms_conditions_title = fields.Html(
+        'General terms and conditions title')
+    general_terms_conditions_left = fields.Html(
+        'General terms and conditions left column')
     general_terms_conditions_right = fields.Html(
         'General terms and conditions right column')
 
     discount_program_ids = fields.One2many('sale.discount.program',
                                            'report_config_id',)
 
-    _sql_constraints = [
-        ('program_type_lang_id_unique',
-         'unique(program_type,lang_id)',
-         'Error, you cannot have multiple entries having the same Program '
-         'type and Language'),
-    ]
+    @api.multi
+    def name_get(self):
+        return [(record.id, "%s (%s)" % (record.name, record.lang_id.name))
+                for record in self]
+
+    @api.constrains('lang_id', 'active')
+    def _check_unique_active_lang(self):
+        import pdb; pdb.set_trace()
+        langs = []
+        for conf in self.with_context(active_test=False).search([]):
+            lang = conf.lang_id
+            if lang in langs and conf.active:
+                raise models.ValidationError(_(
+                    'You cannot have two active models for the same language'))
+            if conf.active:
+                langs.append(lang)
+
+    @api.multi
+    def copy(self, default=None):
+        default['active'] = False
+        return super(SaleDiscountProgramReportConfig, self).copy(default)
+
+    @api.multi
+    def _compute_amount_container_style(self):
+        for conf in self:
+            conf.amount_container_style = ('position: absolute; top: %spx; '
+                                           'left: %spx; height: 80px; width: '
+                                           '%spx; padding-top: 20px; '
+                                           'font-size: 2em; font-weight: '
+                                           'bold; text-align: center;') % (
+                conf.amount_position_top, conf.amount_position_left,
+                conf.amount_zone_width)
