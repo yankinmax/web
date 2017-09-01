0cb991e07325eb549560764e00f59950c5582c0d
jcoux <julien.coux@camptocamp.com>
2017-09-01 14:40:14 +0200

    DT-79: Remove update request (because we will reset integration instance)

--- odoo/songs/updates/update_10_1_2/update.sql
+++ odoo/songs/updates/update_10_1_2/update.sql
@@ -23,15 +23,3 @@ SET name = 'voucher_config_template_fr',
 WHERE module = 'discount_program_report'
 AND name = 'config_template_fr'
 AND model = 'sale.discount.program.report.config';
-
-UPDATE ir_model_data
-SET name = 'gift_card_to_create'
-WHERE module = 'discount_program_report'
-AND name = 'gift_card'
-AND model = 'product.product';
-
-UPDATE ir_model_data
-SET name = 'gift_card_to_create_template'
-WHERE module = 'discount_program_report'
-AND name = 'gift_card_template'
-AND model = 'product.template';
