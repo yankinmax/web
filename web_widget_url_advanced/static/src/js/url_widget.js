6a31c51cc1b4b36447c96ade90747b8df39f8bbe
Simone Orsi <simahawk@gmail.com>
2018-11-23 13:09:07 +0100

    fixup! [ADD] web_widget_url_advanced

--- web_widget_url_advanced/static/src/js/url_widget.js
+++ web_widget_url_advanced/static/src/js/url_widget.js
@@ -13,8 +13,10 @@ odoo.define('web_widget_url_advanced', function (require) {
         init: function () {
             this._super.apply(this, arguments);
             // retrieve customized `<a />` text from a field
-            // via `text_field` attribute
-            var field_value = this.recordData[this.attrs.text_field];
+            // via `text_field` attribute or `options.text_field`
+            // NOTE: `<field text_field="foo"..` is not supported in tree views.
+            var text_field = this.attrs.text_field || this.attrs.options.text_field;
+            var field_value = this.recordData[text_field];
             if (_.isObject(field_value) && _.has(field_value.data)) {
                 field_value = field_value.data.display_name;
             }
