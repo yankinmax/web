57a19f15c0e31dbadee09970b3df508ed90e19bf
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-08-16 11:27:34 +0200

    fixup! Add tr_sequence on res.lang to allow sorting languages in the translation dialog

--- web_translate_dialog/models/res_lang.py
+++ web_translate_dialog/models/res_lang.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 from odoo import models, fields
-from odoo import api
+
 
 class ResLang(models.Model):
 
@@ -12,8 +12,3 @@ class ResLang(models.Model):
         help='Defines the order of language to appear in translation dialog',
         default=10,
     )
-
-    @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
-        print(order)
-        return super().search(args, offset=offset, limit=limit, order=order, count=count)
